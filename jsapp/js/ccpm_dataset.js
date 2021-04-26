a4cbef224ab5979f59f9af759cdfd47d8ce9d5fa
lemoissonM <murhulametre@gmail.com>
2021-04-26 02:28:43 -0400

    CCPM-15 build new report

---
+++ jsapp/js/ccpm_dataset.js
@@ -0,0 +1,101 @@
+const dataset = {
+    supportServiceDelivery: {
+        name: 'Support Service Delivery',
+        code: 'P_SS',
+        comments: ['P_SS05', 'P_SS06'],
+        meeting: {code : '01a', starting: 1, end: 6, name: 'Meeting', noteName: 'Notes from the coordinator', notes: [{code: 'C_SD01', name: 'Meeting Frequency'}, {code: 'C_SD02', name: 'Notes on Meetings'}]},
+        clusterStrategicDecisions : {code : '02', starting: 1, end: 3, name: 'Cluster Strategic Decision',noteName: 'Notes on Strategic Decisions', notes: [{code: 'C_SD03', name: ''}]},
+        clusterMapping: {code: '03', starting: 1, end: 5, name: 'Cluster Mapping', noteName: 'Notes on Mapping', notes: [{code: 'C_SD04', name: ''}]},
+        identificationNeeds: {code: '04', starting: 1, end: 3, name: 'Identification Needs', noteName: 'Notes on identification of Neefs, Gaps and Priorities', notes: [{code: 'C_SD05', name: ''}] },
+    },
+    informingStrategicDecisions: {
+        name: 'Informing Strategic Decisions',
+        code: 'P_IS',
+        comments: ['P_SS06', 'P_SS07'],
+        assesments: {code: '01', starting: 1, end: 7, name: 'Assessments', noteName: '',notes: [{code: 'C_IS01', name: 'Have Assessments, Tools and Guidance been agreed'},{code: 'C_IS02', name: 'Have there been any coordinated assessments (that include a focus on health) over the last year? '}]},
+        situationAnalyses: {code: '02', name: 'Situation Analyses', noteName: '',notes: [{code: 'C_IS03', name: 'Has the cluster done any situation analysis over the last year'}]},
+        analysisTopicCovered: {code: '03', starting: 1, end: 5, name: 'Analysis Topic Covered'},
+        crossCuttingIssues: {code: '04', starting: 1, end: 9, name : 'Cross Cutting Issues'},
+        supportDecisionMaking: {code: '05', starting: 1, end: 2, name: 'Support for Decifion Making', noteName: 'Notes on Informing Strategic Decisions',notes: [{code: 'C_IS04', name: ''}]},
+    },
+    planningStrategyDevelopment: {
+        name: 'Planning Strategy Development',
+        code: 'P_PS',
+        comments: ['P_PS03', 'P_PS04'],
+        strategicPlan: {code: '01b', starting: 1, end: 4, name: 'Strategic Plan', noteName: 'Coordinator Notes',notes: [{code: 'C_PS01', name: 'Coordinator Note: Has the cluster developed a strategic plan?'}]},
+        technicalStandard: {code: '02', starting: 1, end: 2, name: 'Technical Standards and guidelines',noteName: 'Coordinator Notes',notes: [{code: 'C_PS02', name: 'Have technical standards and guidelines been agreed? '}]},
+        prioritizationProposal: {code: '02', starting: 3, end: 5, name: 'Prioritization of proposals'},
+        updateFundingStatus: {code: '02', starting: 6, end: 6, name: 'Updates on funding status against needs', noteName: 'Coordinator Notes',notes: [{code: 'C_PS04', name: 'How often does the cluster report on funding status against needs?'}, {code: 'C_PS05', name: 'Coordinator Notes on Planning and Strategy Development'}]}
+    },
+    advocacy: {
+        code: 'P_AD',
+        name: 'Advocacy',
+        comments: ['P_AD02', 'P_AD03'],
+        identificationAdvocacy: {code: '01', starting: 1, end: 1, name: 'Identification of advocacy issues'},
+        discussionAdvocacyIssue: {code: '01', starting: 2, end: 5, name: 'Discussion on advocacy issues', noteName: '',notes: [{code: 'C_AD01', name: 'Coordinator Notes on Advocacy'}]}
+    },
+    monitoringReporting: {
+        code: 'P_MR',
+        name: 'Monitoring Reporting',
+        comments: ['P_MR02', 'P_MR03'],
+        clusterBulletins: {code: '01', starting: 1, end: 2, name: 'Cluster Bulletins'},
+        programMonitoring: {code: '01', starting: 3, end: 4, name: 'Programme Monitoring and Reporting Formats', noteName: 'Coordinator Notes',notes: [{code: 'C_MR01', name: 'Have programme monitoring and reporting formats been agreed by the cluster?'}]},
+        considerationDiverseNeed: {code: '01', starting: 5, end: 5, name: 'Consideration of diverse needs of women, girls, boys and men in response monitoring', noteName: 'Coordinator Notes on Monitoring and Reporting',notes: [{code: 'C_MR02', name: ''}]}
+    },
+    preparedness: {
+        code: 'P_PR',
+        name: 'Preparedness',
+        comments: ['P_PR02', 'P_PR03'],
+        preparednessPlan: {code: '01', starting: 1, end: 5, name: 'Preparedness Plans', noteName: 'Coordinator Notes',notes: [{code: 'C_PR02', name: 'Is preparedness for recurrent disasters relevant for this cluster?'},{code: 'C_PR01', name: 'Notes on Preparedness for Recurrent Disasters'}]},
+    },
+    aap: {
+        code: 'P_AA',
+        name: 'AAP',
+        comments: ['P_AA02', 'P_AA03'],
+        mechanismForConsulting: {code: '01', starting: 1, end: 2, name: 'Mechanisms for consulting and involving affected populations in decision making', noteName: 'Coordinator Notes',notes: [{code: 'C_AA01', name: 'Has the cluster agreed on mechanisms for consulting and involving affected populations in decision making?'}]},
+        mechanismToReceive: {code: '01', starting: 3, end: 4, name: 'Mechanisms to receive, investigate and act on complaints by affected people', noteName: 'Coordinator Notes',notes: [{code: 'C_AA02', name: 'Coordinator Notes on AAP'}]}
+    }
+}
+
+export const ccpm_getQuestionInRange = (groupIdentifier, subgroupIdentifier) => {
+    const group = dataset[groupIdentifier];
+    const subgroup = group[subgroupIdentifier]
+    const code =  `${group.code}${subgroup.code}`;
+    const questions = [];
+    if(!subgroup.starting) questions.push(code);
+    else{
+    for(let i = subgroup.starting; i <= subgroup.end;i++){
+        questions.push(`${code}_0${i}`);
+    }}
+    return questions;
+}
+
+export const ccpm_getAverageInQuestion = (question) => {
+    if(question){
+    const data  = question.data.responses.reduce((a,b) => Number.parseInt(a.toString().replace(/\D/g,''), 10)+Number.parseInt(b.toString().replace(/\D/g,''), 10), 0);
+    return data/(question.data.responses.length > 0 ?question.data.responses.length : 1);
+    } return 0;
+}
+
+export const ccpm_getAverageInBoolQuestion = (question) => {
+    if(question){
+    const data = question.data.responses.filter(e => e.toString().toLowerCase() === 'yes');
+    return data.length / (question.data.responses.length > 0 ? question.data.responses.length : 1);
+    } 
+    return question;
+}
+
+export const ccpm_getAverageInSubGroup = (data) =>{
+    if(data.length === 0 || !data[0]) return 0;
+    if(data[0].data.responses.includes('Yes') || data[0].data.responses.includes('No')){
+        return data.map(e => ccpm_getAverageInBoolQuestion(e));
+    }
+    const questionsAverage = data.map(e => {
+        return ccpm_getAverageInQuestion(e) 
+    });
+    return questionsAverage.reduce((a,b) => a+b, 0) / data.length;
+}
+
+
+
+export default dataset;
---
+++ jsapp/js/ccpm_report.es6
@@ -0,0 +1,99 @@
+import dataset, {ccpm_getQuestionInRange, ccpm_getAverageInSubGroup, ccpm_getAverageInQuestion, ccpm_getAverageInBoolQuestion} from './ccpm_dataset';
+
+const ccpm_getStatusLabel = (average) => {
+    if(average < 1.25) return 'Weak';
+    if(average < 2.5) return 'Unsatisfactory';
+    if(average < 3.75) return 'Satisfactory';
+    if(average > 3.75) return 'Good'; 
+}
+
+const ccpm_getAverageInquestion = (data) => {
+    if(data.data.responses.includes('Yes') || data.data.responses.includes('No')){
+      return ccpm_getAverageInBoolQuestion(data);
+    } 
+    return ccpm_getAverageInQuestion(data);
+}
+
+const ccpm_getSumOfQuestions = (questions, data) => {
+    const result  = data.filter(e => questions.includes(e.name));
+    return result.reduce((a,b) =>{ 
+        if(!a.data) return b.data.mean;
+        return a.data.mean+b.data.mean}, 0);
+}
+
+const ccpm_getNumberOfParnerResponseByType = (questionList, data) => {
+    const questions  = data.filter(e => questionList.includes(e.name));
+    const disagregatedByType = data.find(e => e.name === 'P_GI03');
+    const result = [];
+    questions.forEach(element => {
+        const elementName = element.row.label[0].replace(' ','').toLowerCase();
+        const index = disagregatedByType.data.responses.findIndex(e => elementName.replace('ngos', 'ngo') === e.replace(' ', '').toLowerCase());
+        result.push({...element, questionsDisagregatedByPartner: disagregatedByType.data.frequencies[index]});
+    })
+    return result;
+}
+
+const ccpm_getResponseGrouped = (q) => {
+    const result = {"1":0, "2": 0, "3": 0, "4": 0, "5": 0};
+    Object.keys(result).forEach(key => {
+        const index = q.data.responses.findIndex(e => e.toString().replace(/\D/g,'') === key);
+        if(index >= 0)
+            result[key] = q.data.frequencies[index];
+    })
+    return result;
+}
+
+
+const ccpm_getData = (data) => {
+    const newReport = {};
+    const chartData = {};
+    const questionResponseGroup = {};
+    Object.keys(dataset).forEach(group => {
+        const groupedByLabel = {"1":0, "2": 0, "3": 0, "4": 0, "5": 0};
+        Object.keys(dataset[group]).forEach(subGroup => {
+            if(subGroup !== 'code' || subGroup !== 'name'){
+            newReport[subGroup] = {};
+            newReport[subGroup].questions = data.filter(e => ccpm_getQuestionInRange(group, subGroup).includes(e.name)).map(q => {
+                q.average = ccpm_getAverageInquestion(q);
+                q.averageLabel = ccpm_getStatusLabel(q.average);
+
+                const questionGroupedByLabel = ccpm_getResponseGrouped(q);
+                Object.keys(questionGroupedByLabel).forEach(v=> {
+                    groupedByLabel[v] = groupedByLabel[v] + questionGroupedByLabel[v];
+                })
+
+                if(!chartData[group]) chartData[group] = {};
+                if(chartData[group][q.averageLabel]) chartData[group][q.averageLabel] = chartData[group][q.averageLabel] + 1;
+                else chartData[group][q.averageLabel] = 1;
+                return q;
+            });
+            newReport[subGroup].averageInGroup = ccpm_getAverageInSubGroup(newReport[subGroup].questions);
+            newReport[subGroup].group = group;
+        }
+        })
+        questionResponseGroup[group] = groupedByLabel;
+    }) 
+    
+    const totalResponseQuestions = []
+    for(let i = 1; i <= 9;i++){
+        totalResponseQuestions.push(`C_CP01_0${i}`);
+    }
+
+    const totalEffectiveResponseQuestions = []
+    for(let i = 1; i <= 9;i++){
+        totalEffectiveResponseQuestions.push(`C_CP02_0${i}`);
+    }
+
+    const typeOfSurvey = data.find(e => e.name === 'type_of_survey');
+    const numberOfPartner = typeOfSurvey.data.frequencies[1];
+
+    const finalData =  {report : newReport, chartData, totalReponses: {numberOfPartner, sum: ccpm_getSumOfQuestions(totalResponseQuestions, data)}, 
+        totalResponseDisagregatedByPartner: ccpm_getNumberOfParnerResponseByType(totalResponseQuestions, data),
+        totalEffectiveResponse : {numberOfPartner, sum : ccpm_getSumOfQuestions(totalEffectiveResponseQuestions, data)},
+        totalEffectiveResponseDisagregatedByPartner : ccpm_getNumberOfParnerResponseByType(totalEffectiveResponseQuestions, data),
+        questionResponseGroup: questionResponseGroup
+        }
+    return finalData;
+    }
+
+export default ccpm_getData;
