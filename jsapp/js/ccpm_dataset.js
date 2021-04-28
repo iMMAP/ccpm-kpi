a355bf66ccc02076411006d4c0416fba14cdbfcb
Murhula Metre Lemoisson <49560334+lemoissonM@users.noreply.github.com>
2021-04-28 16:25:13 -0400

    Update jsapp/js/ccpm_dataset.js
    Co-authored-by: Deepak Jeswani <djeswani@immap.org>

--- jsapp/js/ccpm_dataset.js
+++ jsapp/js/ccpm_dataset.js
@@ -4,7 +4,7 @@ const dataset = {
         code: 'P_SS',
         comments: ['P_SS05', 'P_SS06'],
         meeting: {code : '01a', starting: 1, end: 6, name: 'Meetings', noteName: 'Notes from the Coordinator', notes: [{code: 'C_SD01', name: 'Meeting Frequency'}, {code: 'C_SD02', name: 'Notes on Meetings'}]},
-        clusterStrategicDecisions : {code : '02', starting: 1, end: 3, name: 'Cluster Strategic Decision',noteName: 'Notes on Strategic Decisions', notes: [{code: 'C_SD03', name: ''}]},
+        clusterStrategicDecisions : {code : '02', starting: 1, end: 3, name: 'Cluster Strategic Decisions',noteName: 'Notes on Strategic Decisions', notes: [{code: 'C_SD03', name: ''}]},
         clusterMapping: {code: '03', starting: 1, end: 5, name: 'Cluster Mapping', noteName: 'Notes on Mapping', notes: [{code: 'C_SD04', name: ''}]},
         identificationNeeds: {code: '04', starting: 1, end: 3, name: 'Identification Needs', noteName: 'Notes on identification of Neefs, Gaps and Priorities', notes: [{code: 'C_SD05', name: ''}] },
     },
